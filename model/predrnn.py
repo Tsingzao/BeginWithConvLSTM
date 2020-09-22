e3f692050d9cb8afcf41ad3abdffaa55fb8634bd
yutingzhao <tsingzao@hotmail.com>
2020-09-22 18:17:54 +0800

    20200922 1800

---
+++ model/predrnn.py
@@ -0,0 +1,135 @@
+# forked from https://github.com/thuml/predrnn-pytorch
+import torch
+import torch.nn as nn
+
+class SpatioTemporalLSTMCell(nn.Module):
+    def __init__(self, in_channel, num_hidden, width, filter_size, stride):
+        super(SpatioTemporalLSTMCell, self).__init__()
+
+        self.num_hidden = num_hidden
+        self.padding = filter_size // 2
+        self._forget_bias = 1.0
+        self.conv_x = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden * 7, kernel_size=filter_size, stride=stride, padding=self.padding),
+            nn.LayerNorm([num_hidden * 7, width, width])
+        )
+        self.conv_h = nn.Sequential(
+            nn.Conv2d(num_hidden, num_hidden * 4, kernel_size=filter_size, stride=stride, padding=self.padding),
+            nn.LayerNorm([num_hidden * 4, width, width])
+        )
+        self.conv_m = nn.Sequential(
+            nn.Conv2d(num_hidden, num_hidden * 3, kernel_size=filter_size, stride=stride, padding=self.padding),
+            nn.LayerNorm([num_hidden * 3, width, width])
+        )
+        self.conv_o = nn.Sequential(
+            nn.Conv2d(num_hidden * 2, num_hidden, kernel_size=filter_size, stride=stride, padding=self.padding),
+            nn.LayerNorm([num_hidden, width, width])
+        )
+        self.conv_last = nn.Conv2d(num_hidden * 2, num_hidden, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self, x_t, h_t, c_t, m_t):
+        x_concat = self.conv_x(x_t)
+        h_concat = self.conv_h(h_t)
+        m_concat = self.conv_m(m_t)
+        i_x, f_x, g_x, i_x_prime, f_x_prime, g_x_prime, o_x = torch.split(x_concat, self.num_hidden, dim=1)
+        i_h, f_h, g_h, o_h = torch.split(h_concat, self.num_hidden, dim=1)
+        i_m, f_m, g_m = torch.split(m_concat, self.num_hidden, dim=1)
+
+        i_t = torch.sigmoid(i_x + i_h)
+        f_t = torch.sigmoid(f_x + f_h + self._forget_bias)
+        g_t = torch.tanh(g_x + g_h)
+
+        c_new = f_t * c_t + i_t * g_t
+
+        i_t_prime = torch.sigmoid(i_x_prime + i_m)
+        f_t_prime = torch.sigmoid(f_x_prime + f_m + self._forget_bias)
+        g_t_prime = torch.tanh(g_x_prime + g_m)
+
+        m_new = f_t_prime * m_t + i_t_prime * g_t_prime
+
+        mem = torch.cat((c_new, m_new), 1)
+        o_t = torch.sigmoid(o_x + o_h + self.conv_o(mem))
+        h_new = o_t * torch.tanh(self.conv_last(mem))
+
+        return h_new, c_new, m_new
+
+
+class PredRNN(nn.Module):
+    def __init__(self, num_layers, num_hidden, configs):
+        super(PredRNN, self).__init__()
+
+        self.configs = configs
+        self.frame_channel = configs.patch_size * configs.patch_size
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        cell_list = []
+
+        width = configs.img_width // configs.patch_size
+
+        for i in range(num_layers):
+            in_channel = self.frame_channel if i == 0 else num_hidden[i-1]
+            cell_list.append(
+                SpatioTemporalLSTMCell(in_channel, num_hidden[i], width, configs.filter_size, configs.stride)
+            )
+        self.cell_list = nn.ModuleList(cell_list)
+        self.conv_last = nn.Conv2d(num_hidden[num_layers-1], self.frame_channel,
+                                   kernel_size=1, stride=1, padding=0, bias=False)
+
+
+    def forward(self, frames, mask_true):
+        # [batch, length, height, width, channel] -> [batch, length, channel, height, width]
+        frames = frames.permute(0, 1, 4, 2, 3).contiguous()
+        mask_true = mask_true.permute(0, 1, 4, 2, 3).contiguous()
+
+        batch = frames.shape[0]
+        height = frames.shape[3]
+        width = frames.shape[4]
+
+        next_frames = []
+        h_t = []
+        c_t = []
+
+        for i in range(self.num_layers):
+            zeros = torch.zeros([batch, self.num_hidden[i], height, width]).to(self.configs.device)
+            h_t.append(zeros)
+            c_t.append(zeros)
+
+        memory = torch.zeros([batch, self.num_hidden[0], height, width]).to(self.configs.device)
+
+        for t in range(self.configs.total_length-1):
+            if t < self.configs.input_length:
+                net = frames[:,t]
+            else:
+                net = mask_true[:, t - self.configs.input_length] * frames[:, t] + \
+                      (1 - mask_true[:, t - self.configs.input_length]) * x_gen
+
+            h_t[0], c_t[0], memory = self.cell_list[0](net, h_t[0], c_t[0], memory)
+
+            for i in range(1, self.num_layers):
+                h_t[i], c_t[i], memory = self.cell_list[i](h_t[i - 1], h_t[i], c_t[i], memory)
+
+            x_gen = self.conv_last(h_t[self.num_layers-1])
+            next_frames.append(x_gen)
+
+        # [length, batch, channel, height, width] -> [batch, length, height, width, channel]
+        next_frames = torch.stack(next_frames, dim=0).permute(1, 0, 3, 4, 2).contiguous()
+
+        return next_frames
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda:0')
+    from easydict import EasyDict as edict
+    configs = edict()
+    configs.patch_size = 4
+    configs.img_width = 64
+    configs.filter_size = 5
+    configs.stride = 1
+    configs.total_length = 6
+    configs.input_length = 3
+    configs.device = device
+    model = PredRNN(2, [32, 32], configs).float().to(device)
+    input = torch.randn((1,6,16,16,16)).float().to(device)
+    output = model(input, input)
+    print(output.shape)
